dda984fec5994bfda4470ca63fdd3a213e4f543a
sojib-sudo <sojibmandol@gmail.com>
2023-01-23 01:25:31 +0600

    React pagination added

--- src/components/information/Information.js
+++ src/components/information/Information.js
@@ -1,26 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import TableInfo from '../tableInfo/TableInfo';
 
 const Information = () => {
 
     const [informations, setInformations] = useState([]);
 
-
+    // fatching the fake data from a public json file
     useEffect(() => {
         fetch('fakeData.json')
         .then(res => res.json())
         .then(data =>  setInformations(data))
    },[])
 
+
+   // ------------ for pagination 
+   const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCout] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 3;
+
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+        setCurrentItems(informations.slice(itemOffset, endOffset));
+        setPageCout(Math.ceil(informations.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage, informations]);
+
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % informations.length;
+        setItemOffset(newOffset);
+    };
+
     return (
+        <>
         <div className='bg-blue-100  p-5 rounded'>
             {
-                informations.map(info => <TableInfo
+                currentItems.map(info => <TableInfo
                     key={info.id}
                     info={info}
                 ></TableInfo>)
             }
         </div>
+        
+        
+        <ReactPaginate
+                breakLabel="..."
+                nextLabel=" >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                pageCount={pageCount}
+                previousLabel="< "
+                renderOnZeroPageCount={null}
+            />
+        </>
     );
 };
 
--- src/components/tableInfo/TableInfo.js
+++ src/components/tableInfo/TableInfo.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 const TableInfo = ({info}) => {
     const {name, contact, city, state, details, id} = info;
+
+
     return (
-        <div className='bg-white p-5 rounded m-5 grid grid-cols-5'>
-            <div className='my-auto'>
-                <h3>{name}</h3>
+        <>
+
+            <div className='bg-white p-5 rounded m-5 grid grid-cols-5'>
+                <div className='my-auto'>
+                    <h3>{name}</h3>
+                </div>
+                <div>
+                    <h2>CONTACT</h2>
+                    <h3>{contact}</h3>
+                </div>
+                <div>
+                    <h2>CITY</h2>
+                    <h3>{city}</h3>
+                </div>
+                <div>
+                    <h2>STATE</h2>
+                    <h3>{state}</h3>
+                </div>
+                <div className=''>
+                    <button className="btn btn-error rounded-full ">View Details</button>
+                </div>
             </div>
-            <div>
-                <h2>CONTACT</h2>
-                <h3>{contact}</h3>
-            </div>
-            <div>
-                <h2>CITY</h2>
-                <h3>{city}</h3>
-            </div>
-            <div>
-                <h2>STATE</h2>
-                <h3>{state}</h3>
-            </div>
-            <div className=''>
-                <button className="btn btn-error rounded-full ">View Details</button>
-            </div>
-        </div>
+            
+        </>
     );
 };
 
